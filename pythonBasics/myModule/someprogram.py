bca7ef5b3f35d4946688f59ad818d8f0f68cf97a
Yael Brown <yaelrbrown@gmail.com>
2020-01-07 23:54:22 -0600

    Added notes on pypi, also how to create modules and packages with example.

---
+++ pythonBasics/myModule/someprogram.py
@@ -0,0 +1,11 @@
+# import from module
+from mymodule import my_func
+
+# import from module
+from MyMainPackage import somescript
+from MyMainPackage.SubPackage import somesubscript
+
+my_func()
+
+somescript.report_main()
+somesubscript.sub_report()
