be5ea2b1e13953b19e3e6bb9be742be79b1fcd9e
Yael Brown <yaelrbrown@gmail.com>
2020-02-26 21:01:33 -0600

    added socket implementation

---
+++ usefulPythonFiles/socket-server.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1024))
+s.listen(5)
+
+while True:
+  clt, adr = s.accept()
+  print(f"Connection to {adr} established")
+  clt.send(bytes("Socket Programming in Python", "utf-8"))
