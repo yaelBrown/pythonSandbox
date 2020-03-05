f8da7747a3108a033c65d1b9456a76fbd6ea9789
Yael Brown <yaelrbrown@gmail.com>
2020-03-04 19:59:29 -0600

    cleaned code up a little bit....

--- usefulPythonFiles/workingSocket-impl/js-client.js
+++ usefulPythonFiles/workingSocket-impl/js-client.js
@@ -1,22 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("trying to connect")
+    console.log("trying to connect... ")
     var socket = io("http://localhost:5000");
+
     socket.on('connect', () => {
         socket.send("I am connected");
     });
+
     socket.on('message', data => {
         console.log("Message recieved: " + data);
     })
-
-    // socket.on('my response', function(msg) {
-    //     $('#log').append('<p>Received: ' + msg.data + '</p>');
-    // });
-    // $('form#emit').submit(function(event) {
-    //     socket.emit('my event', {data: $('#emit_data').val()});
-    //     return false;
-    // });
-    // $('form#broadcast').submit(function(event) {
-    //     socket.emit('my broadcast event', {data: $('#broadcast_data').val()});
-    //     return false;
-    // });
 })
--- usefulPythonFiles/workingSocket-impl/python-server.py
+++ usefulPythonFiles/workingSocket-impl/python-server.py
@@ -13,18 +13,18 @@ def index():
 
 
 
+@socketio.on('message')
+def message(data):
+  print(f"{data}")
+  send(data)
+
 @socketio.on('my event', namespace='/test')
 def test_message(message):
     emit('my response', {'data': message['data']})
 
 @socketio.on('my broadcast event', namespace='/test')
-def test_message(message):
+def test_message_broadcast(message):
     emit('my response', {'data': message['data']}, broadcast=True)
-
-@socketio.on('message')
-def message(data):
-  print(f"{data}")
-  send(data)
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
@@ -37,5 +37,3 @@ def test_disconnect():
 if __name__ == '__main__':
     print("Running server")
     socketio.run(app, host="0.0.0.0")
-
-# https://www.google.com/search?rlz=1C1GCEU_enUS884US884&sxsrf=ALeKk01uX_g4OU1_0UcORSDJZjBYKQGyNA%3A1582827253128&ei=9QZYXt-wB8fUtQXR6oXoDA&q=create+socket+with+socketio+flask&oq=create+socket+with+socketio+flask&gs_l=psy-ab.3..33i22i29i30.5748.7317..7465...1.2..1.203.951.0j6j1....2..0....1..gws-wiz.......0i71j33i22i29i30i395j33i10i160j33i10i299.Bl3H_rAxXnc&ved=0ahUKEwif_6aBq_LnAhVHaq0KHVF1Ac0Q4dUDCAs&uact=5#kpvalbx=_CAdYXqGaJ4yEtQXAurDIDQ20
