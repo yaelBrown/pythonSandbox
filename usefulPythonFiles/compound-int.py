fe4b4a13400e17c4fd276a5fad5b1f8f29498bc8
Yael Brown <yaelrbrown@gmail.com>
2020-04-02 01:27:17 -0500

    added file to send-email

--- usefulPythonFiles/compound-int.py
+++ usefulPythonFiles/compound-int.py
@@ -1,12 +1,9 @@
-
-
 initial = 10000
 cnt = 1
 maxx = 20000
 
 def compound(i, p):
   return (i * p) + i
-
 
 curAmt = initial
 while curAmt < maxx:
---
+++ usefulPythonFiles/send-email.py
@@ -0,0 +1,29 @@
+import smtplib
+
+from email.message import Message
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+email = "yaelrbrowndev@gmail.com"
+password = ""
+
+send_to_email = "yaelrbrown@gmail.com"
+subject = "Email from Python"
+message = "This is a message"
+
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = send_to_email
+msg['Subject'] = subject
+
+msg.attach(MIMEText(message, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email, password)
+text = msg.as_string()
+server.sendmail(email, send_to_email, text)
+server.quit()
