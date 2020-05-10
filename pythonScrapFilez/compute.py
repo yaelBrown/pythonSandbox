fe725f01eb4c666f5022ece9a2cec987bf01bcef
Yael Brown <yaelrbrown@gmail.com>
2020-05-10 16:34:52 -0500

    Added math 140 applied project file using linear regression formulas

--- pythonScrapFilez/compute.py
+++ pythonScrapFilez/compute.py
@@ -1,10 +1,10 @@
 print("hello")
 
-cnt = 400
+cnt = 600
 divByFour = 0
-while cnt != 199:
-  if cnt % 4 == 0:
+while cnt != 299:
+  if cnt % 3 == 0:
     divByFour += 1
   cnt -= 1
 
-print(f"{divByFour} numbers are divisible by 4")
+print(f"{divByFour} numbers are divisible by 3")
