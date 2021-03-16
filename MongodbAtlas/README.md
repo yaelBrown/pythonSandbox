42d0f810fae98139892a2d5656cc8479f8d116df
Yael Brown <yaelrbrown@gmail.com>
2021-03-15 21:47:48 -0500

    adding mongo example

--- MongodbAtlas/README.md
+++ MongodbAtlas/README.md
@@ -1,3 +1,6 @@
 Doing CRUD operations with a mongodb atlas (free) acct
 
 https://www.youtube.com/watch?v=VQnmcBnguPY
+
+
+sudo /Applications/Python\ 3.8/Install\ Certificates.command
---
+++ MongodbAtlas/atlas_app.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+# print("atlas app file is working")
+
+client = MongoClient("mongodb+srv://pet_owner:pets@peterest-db.fkffd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.get_database('Peterest_db')
+users_db = db.users
+
+print(users_db.count_documents({}))
