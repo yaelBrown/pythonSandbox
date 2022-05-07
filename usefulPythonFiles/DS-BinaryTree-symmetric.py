01ae82c184b966f5f6e328b48485ee5a20cb8614
yaelBrown <yaelrbrown@gmail.com>
2022-05-07 11:42:49 -0500

    add: binaryTree symetric

--- usefulPythonFiles/DS-BinaryTree-symmetric.py
+++ usefulPythonFiles/DS-BinaryTree-symmetric.py
@@ -6,4 +6,14 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        
+        if not root.left and not root.right: return False
+        if not root.left or not root.right: return False
+
+        j, k = root.left, root.right
+        q = []
+        sym = True
+        while sym:
+            if j.left:
+                if not k.right: 
+                    return False
+            
