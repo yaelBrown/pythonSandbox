9e8cd76dd94185a096b34ff27664f6b16f690bfe
Yael Brown <yaelrbrown@gmail.com>
2025-02-21 07:58:24 -0500

    add: add exponential decline script

---
+++ pythonScripts/exp_decline.py
@@ -0,0 +1,11 @@
+def exponentialDecline(val, itr):
+    while itr > 0: 
+        val = val / 2
+        print(f"{itr}: {val}")
+        itr = itr - 1 
+        
+        
+        
+        
+exponentialDecline(33301.96, 4)
+    
